f4f207a4bf7ce0522a05d4832f7b3255d9221068
Alice Smith <smi17100@byui.edu>
2019-05-08 17:04:35 -0600

    indentation correction

--- src/medTracker/Main.java
+++ src/medTracker/Main.java
@@ -1,8 +1,8 @@
 package medTracker;
 
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
+  
+  public static void main(String[] args) {
+    // write your code here
+  }
 }
