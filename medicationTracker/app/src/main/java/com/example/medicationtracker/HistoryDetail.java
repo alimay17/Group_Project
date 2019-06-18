09d874cde6b250a1d454f989e7c4d18af8bb7ab7
Alice Smith <smi17100@byui.edu>
2019-06-18 17:52:32 -0600

    Basic navigation is in place

--- medicationTracker/app/src/main/java/com/example/medicationtracker/HistoryDetail.java
+++ medicationTracker/app/src/main/java/com/example/medicationtracker/HistoryDetail.java
@@ -2,6 +2,7 @@ package com.example.medicationtracker;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +15,27 @@ public class HistoryDetail extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_history_detail);
     Log.d(TAG, "This is History Detail");
+    getIncomingIntent();
+  }
+
+  private void getIncomingIntent() {
+    Log.d(TAG, "getIncomingIntent: Getting intent");
+
+    if(getIntent().hasExtra("medication")) {
+      Log.d(TAG, "getIncomingIntent: Got intent");
+      String medName = getIntent().getStringExtra("medication");
+      displayImage(medName);
+
+    }
+    else {
+      Log.d(TAG, "getIncomingIntent: no Intent");
+    }
+  }
+
+  private void displayImage(String medName) {
+    Log.d(TAG, "displayImage: setting med to display");
+    TextView name = findViewById(R.id.medDetail);
+    name.setText("Your Selected Medication: " + medName);
   }
 
 }
