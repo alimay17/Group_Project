1e18ccf9370c82e20e54c6c96666c5fc1a2173c7
Alice Smith <smi17100@byui.edu>
2019-07-02 18:36:15 -0600

    basic date functionality added. need to perfect

--- medicationTracker/app/src/main/java/com/example/medicationtracker/MedRoomDatabase.java
+++ medicationTracker/app/src/main/java/com/example/medicationtracker/MedRoomDatabase.java
@@ -6,9 +6,11 @@ import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Medication.class}, version = 1)
+@TypeConverters({Converters.class})
 public abstract class MedRoomDatabase extends RoomDatabase {
 
   public abstract MedDao medDao();
--- medicationTracker/app/src/main/java/com/example/medicationtracker/Medication.java
+++ medicationTracker/app/src/main/java/com/example/medicationtracker/Medication.java
@@ -1,9 +1,9 @@
 package com.example.medicationtracker;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
-import androidx.annotation.NonNull;
 
 // Annotations are for the db functionality.
 @Entity(tableName = "medication")
@@ -15,13 +15,14 @@ public class Medication {
 
   @ColumnInfo(name = "med_name")
   private String mName;
-
   private String dose;
+  private Long created;
 
 
   public Medication(String mName, String dose) {
     this.mName = mName;
     this.dose = dose;
+    created = System.currentTimeMillis();
   }
 
 
@@ -29,9 +30,11 @@ public class Medication {
   public int getId() { return this.id; }
   public String getName() { return this.mName; }
   public String getDose() { return dose; }
+  public Long getCreated() { return created; }
 
   // setters
   public void setId(int id) { this.id = id; }
   public void setmName(@NonNull String mName) { this.mName = mName; }
   public void setDose(String dose) { this.dose = dose; }
+  public void setCreated(Long created) { this.created = created; }
 }
