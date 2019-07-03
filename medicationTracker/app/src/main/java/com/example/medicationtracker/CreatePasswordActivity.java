aafcd1ab8d1902c9f8f4a6cf93cca3f9b92e06b3
Rich <rgterry@gmail.com>
2019-07-02 23:35:10 -0600

    Auto stash before rebase of "origin/master"
    Large Re-design on the enter/create password

--- medicationTracker/app/src/main/java/com/example/medicationtracker/CreatePasswordActivity.java
+++ medicationTracker/app/src/main/java/com/example/medicationtracker/CreatePasswordActivity.java
@@ -5,51 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class CreatePasswordActivity extends AppCompatActivity {
-    EditText editText, editText2;
-    Button button;
+    public static final String TAG = "CreatePasswordActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creat_password);
-
-        editText = (EditText) findViewById(R.id.editText);
-        editText2 = (EditText) findViewById(R.id.editText2);
-        button = (Button) findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text1 = editText.getText().toString();
-                String text2 = editText2.getText().toString();
-
-                if(text1.equals("") || text2.equals("")){
-                    //No password
-                    Toast.makeText(CreatePasswordActivity.this, "No Password Entered", Toast.LENGTH_SHORT).show();
-                }else {
-                    if(text1.equals(text2)){
-                        // Saving the password to Shared Preferences
-                        SharedPreferences settings = getSharedPreferences("PREFS", 0);
-                        SharedPreferences.Editor editor = settings.edit();
-                        editor.putString("password", text1);
-                        editor.apply();
-
-                        // Password is good, go to main activity
-                        Intent intent = new Intent (getApplicationContext(), MainActivity.class);
-                        startActivity(intent);
-                        finish();
-
-                    } else {
-                        Toast.makeText(CreatePasswordActivity.this, "Password does not match!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
     }
+
+    /**
+     *
+     * @param view
+     */
+    public void createPassword(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText newPassword = findViewById(R.id.userPassword);
+        EditText newPasswordVerify = findViewById(R.id.userPasswordVerify);
+        String newPWD = newPassword.getText().toString();
+        String pwdVerify = newPasswordVerify.getText().toString();
+        if(newPWD.equals("") || pwdVerify.equals("")){
+            //No password
+            Toast.makeText(CreatePasswordActivity.this, "No Password Entered", Toast.LENGTH_SHORT).show();
+        } else if(newPWD.length() < 4) {
+                Toast.makeText(this, "Minimum Length is 4 Characters", Toast.LENGTH_SHORT).show();
+        } else{
+            if (newPWD.equals(pwdVerify)) {
+                // Saving the password to Shared Preferences
+                SharedPreferences settings = getSharedPreferences("PREFS", 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("password", newPWD);
+                editor.apply();
+                startActivity(intent);
+                finish();
+                Log.d(TAG, "createPassword: New Password Set");
+
+            } else {
+                Toast.makeText(CreatePasswordActivity.this, "Password does not match!", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "createPassword: Passwords Didn't Match");
+            }
+        }
+
+    }
+
 }
--- medicationTracker/app/src/main/java/com/example/medicationtracker/EnterPasswordActivity.java
+++ medicationTracker/app/src/main/java/com/example/medicationtracker/EnterPasswordActivity.java
@@ -5,44 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.view.View;
+import android.util.Log;
 
+/**
+ *
+ */
 public class EnterPasswordActivity extends AppCompatActivity {
-
-    EditText editText;
-    Button button;
+    public static final String TAG = "EnterPasswordActivity";
     String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_password);
+    }
 
-        // load the password
+    /**
+     *
+     * @param view
+     */
+    public void enterPassword(View view){
         SharedPreferences settings = getSharedPreferences("PREFS", 0);
         password = settings.getString("password", "");
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText userPassword = findViewById(R.id.userPassword);
+        String tempPassword = userPassword.getText().toString();
+        if (tempPassword.equals(password)){
+            startActivity(intent);
+            finish();
+            Log.d(TAG, "enterPassword: Password Matched");
+        }
+        else {
+            Toast.makeText(EnterPasswordActivity.this, "Password does not match!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-        editText = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text = editText.getText().toString();
-
-                if(text.equals(password)){
-                    // Go to main activity
-                    Intent intent = new Intent (getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Toast.makeText(EnterPasswordActivity.this, "Password does not match!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+    /**
+     *
+     * @param view
+     */
+    public void reSetPassword(View view){
+        SharedPreferences settings = getSharedPreferences("PREFS", 0);
+        password = settings.getString("password", "");
+        Intent intent = new Intent(this, CreatePasswordActivity.class);
+        EditText userPassword = findViewById(R.id.userPassword);
+        String tempPassword = userPassword.getText().toString();
+        if (tempPassword.equals(password)){
+            startActivity(intent);
+            finish();
+            Log.d(TAG, "enterPassword: Password Matched now re-setting password");
+        }
+        else {
+            Toast.makeText(EnterPasswordActivity.this, "Password does not match!", Toast.LENGTH_SHORT).show();
+        }
 
     }
+
 }
