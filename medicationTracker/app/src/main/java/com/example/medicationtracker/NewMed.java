8720579c9da44790be521277dc1112e3b20499d9
Eric Mott <mott.eric@sbcglobal.net>
2019-07-09 17:52:08 -0700

    Alarm Stuff

--- medicationTracker/app/src/main/java/com/example/medicationtracker/NewMed.java
+++ medicationTracker/app/src/main/java/com/example/medicationtracker/NewMed.java
@@ -6,21 +6,34 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TimePicker;
+import android.widget.*;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
+import org.w3c.dom.Text;
 
 
-public class NewMed extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+public class NewMed extends AppCompatActivity /*implements TimePickerDialog.OnTimeSetListener*/ {
 
-  public static final String TAG = "NewMed";
+  public static final String TAG = "NEW_MED";
   public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
+  public static final int SET_ALARM_REQUEST_CODE = 5;
   private EditText medName;
   private EditText medDose;
+
+  public void setTextView(String textView) {
+//    mTextView = textView;
+    mTextView.setText(textView);
+  }
+
+  private TextView mTextView;
+
+  public void setAlarmID(int alarmID) {
+    this.alarmID = alarmID;
+  }
+
+  private int alarmID;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +45,34 @@ public class NewMed extends AppCompatActivity implements TimePickerDialog.OnTime
 
     medName = findViewById(R.id.name);
     medDose = findViewById(R.id.dose);
+    mTextView = findViewById(R.id.textView_remindTime);
 
     Button buttonRemind = (Button) findViewById(R.id.button_setReminder);
     buttonRemind.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        DialogFragment timePicker = new TimePickerFragment();
-        timePicker.show(getSupportFragmentManager(), "time picker");
+        if (medName.getText().toString().equals("")) {
+          Toast.makeText(NewMed.this, "Please add a medication", Toast.LENGTH_SHORT).show();
+        } else {
+          Log.d(TAG, "onClick: Creating Intent before calling SetAlarm");
+          Intent intent = new Intent(NewMed.this, SetAlarm.class);
+
+          //add cancel info ID here as:
+          intent.putExtra("medName", medName.getText().toString());
+          intent.putExtra("medDose", medDose.getText().toString());
+          //intent.putExtra(alarmID, alarmID);
+
+          Log.d(TAG, "onClick: Intent created, calling startActivityForResult");
+          startActivityForResult(intent, SET_ALARM_REQUEST_CODE);  // 1 should be replaced with a constant
+
+          // catching returned data
+          Intent incomingIntent = getIntent();
+          setAlarmID(Integer.parseInt(incomingIntent.getStringExtra("medName"))); // convert alarmID string to int
+          setTextView(incomingIntent.getStringExtra("mTextView"));
+        }
       }
     });
+    mTextView.setText("test");
 
     final Button button = findViewById(R.id.button_save);
     button.setOnClickListener(new View.OnClickListener() {
@@ -60,15 +92,29 @@ public class NewMed extends AppCompatActivity implements TimePickerDialog.OnTime
           replyIntent.putExtra("dose", dose);
           setResult(RESULT_OK, replyIntent);
         }
-        Log.d(TAG, "onClick: added new med");
+        Log.d(TAG, "onClick: *****************added new med**************************************************");
         finish();
       }
     });
   }
 
   @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == 1) {
+      if (resultCode == RESULT_OK){
+        alarmID = data.getIntExtra("alarmID", 0);
+      }
+      if (resultCode == RESULT_CANCELED){
+        alarmID = 0;
+      }
+    }
+  }
+
+  /*@Override
   public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
     TextView textView = (TextView)findViewById(R.id.textView_remindTime);
     textView.setText("Hour " + hourOfDay + " Minute: " + minute);
-  }
+  }*/
 }
---
+++ medicationTracker/app/src/main/java/com/example/medicationtracker/NotificationHelper.java
@@ -0,0 +1,55 @@
+package com.example.medicationtracker;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Build;
+import android.util.Log;
+import androidx.core.app.NotificationCompat;
+
+public class NotificationHelper extends ContextWrapper {
+    public static final String TAG = "NotificationHelper";
+    public static final String channel1ID = "channel1ID";
+    public static final String channel1Name = "Medication Reminder";  // this is what the user sees in the user settings
+
+    private NotificationManager mManager;
+
+    public NotificationHelper(Context base) {
+        super(base);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            createChannels();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    public void createChannels(){
+        Log.d(TAG, "createChannels: In NotificationHelper");
+        NotificationChannel channel1 = new NotificationChannel(channel1ID, channel1Name, NotificationManager.IMPORTANCE_DEFAULT);
+        channel1.enableLights(true);
+        channel1.enableVibration(true);
+        channel1.setLightColor(R.color.colorPrimary);
+        channel1.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(channel1);
+    }
+
+    public NotificationManager getManager(){
+        if (mManager == null) {
+            Log.d(TAG, "getManager: mManager was null, creating new mManager");
+            mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        return mManager;
+    }
+
+    public NotificationCompat.Builder getChannel1Notification(String title, String message){
+        Log.d(TAG, "getChannel1Notification: returning new NotificationCompat.Builder");
+        return new NotificationCompat.Builder(getApplicationContext(), channel1ID)
+          .setContentTitle(title)
+          .setContentText(message)
+          .setSmallIcon(R.drawable.ic_med);
+    }
+}
